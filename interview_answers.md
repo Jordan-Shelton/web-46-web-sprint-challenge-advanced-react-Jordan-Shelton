24b58ca401780a708a602214c0a7ccd2538de391
Jordan-Shelton <jordansheltonwork@gmail.com>
2021-09-03 13:36:29 -0500

    finished the mvp

--- interview_answers.md
+++ interview_answers.md
@@ -3,8 +3,16 @@ Be prepared to demonstrate your understanding of this week's concepts by answeri
 
 1. What are the main differences between a stateful and a functional component?
 
+Answer 1. A stateful component has state in it and can keep track of data through state. Making it able to update what is rendered based off of the state. A functional component/stateless component is either a static render where nothing will ever change without hard coding it to or through props.
+
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
+
+Answer 2. ComponentWillMount occurs just before the render. ComponentWillUpdate occurs before render and right after componentShouldUpdate
 
 3. Define stateful logic.
 
+Answer 3. Any code that uses state.
+
 4. What are the three step of creating a successful test? What is done in each phase?
+
+Answer 4.
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,36 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("form header renders", () => {
+    <CheckoutForm />
+});
 
-test("shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(
+        <CheckoutForm />
+    )
+
+    const firstName = screen.getByLabelText(/first name/i)
+    const lastName = screen.getByLabelText(/last name/i)
+    const address = screen.getByLabelText(/address/i)
+    const city = screen.getByLabelText(/city/i)
+    const zip = screen.getByLabelText(/zip/i)
+    const state = screen.getByLabelText(/state/i)
+    const button = screen.getByRole('button')
+
+    userEvent.type(firstName, 'Jordan')
+    userEvent.type(lastName, 'Shelton')
+    userEvent.type(address, '20558 Legacy Xing')
+    userEvent.type(city, 'Harrah')
+    userEvent.type(zip, '73045')
+    userEvent.type(state, 'Oklahoma')
+    userEvent.click(button)
+
+    const message = screen.getByTestId(/successMessage/i)
+    expect(message).toBeInTheDocument()
+    console.log(message)
+});
